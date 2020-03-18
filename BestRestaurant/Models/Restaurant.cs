3ef5be5f7c5b80d950f79dad93b04d23cebd2a7c
Dana <dcluukko@gmail.com>
2020-03-18 15:14:04 -0700

    successfully build controllers, models, and views files
    Co-authored-by: Jeremy Kale Padot <jeremy@padot.us>
    Co-authored-by: Patrick Delaney <Prestwick97@gmail.com>

--- BestRestaurant/Models/Restaurant.cs
+++ BestRestaurant/Models/Restaurant.cs
@@ -1,1 +1,10 @@
-// comment for commit
+namespace BestRestaurant.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Description { get; set; }
+    public int CuisineId { get; set; }
+    public virtual Cuisine Cuisine { get; set; }
+  }
+}
